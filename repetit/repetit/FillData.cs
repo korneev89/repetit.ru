474cad3138db29c776adf77091d820c86e94af41
Dmitry Korneev <31965644+korneev89@users.noreply.github.com>
2018-03-16 08:46:38 +0300

    add tools class

--- repetit/repetit/FillData.cs
+++ repetit/repetit/FillData.cs
@@ -80,76 +80,6 @@ namespace repetit
 			}
 		}
 
-		private static DateTime NextMonday()
-		{
-			DateTime today = DateTime.Today;
-			int daysUntilTuesday = ((int)DayOfWeek.Monday - (int)today.DayOfWeek + 7) % 7;
-			return today.AddDays(daysUntilTuesday);
-		}
-
-
-		private List<Pupil> CreatePupils()
-		{
-			var pupils = new List<Pupil>();
-			var p1 = new Pupil
-			{
-				Name = "Захар (Парк Победы)",
-				DayOfWeek = 3,
-				TimeStart = 20
-			};
-
-			var p2 = new Pupil
-			{
-				Name = "Егор (Павелецкая)",
-				DayOfWeek = 5,
-				TimeStart = 20
-			};
-
-			var p3 = new Pupil
-			{
-				Name = "Дарья (Skype)",
-				DayOfWeek = 7,
-				TimeStart = 12
-			};
-
-			var p4 = new Pupil
-			{
-				Name = "Эмиль (Маяковская)",
-				DayOfWeek = 7,
-				TimeStart = 17
-			};
-
-			var p5 = new Pupil
-			{
-				Name = "Максим (Немчиновка)",
-				DayOfWeek = 6,
-				TimeStart = 12
-			};
-
-			var p6 = new Pupil
-			{
-				Name = "Станислав (Немчиновка)",
-				DayOfWeek = 6,
-				TimeStart = 15
-			};
-
-			var p7 = new Pupil
-			{
-				Name = "Данил (Немчиновка)",
-				DayOfWeek = 6,
-				TimeStart = 16
-			};
-
-			pupils.Add(p1);
-			pupils.Add(p2);
-			pupils.Add(p3);
-			pupils.Add(p4);
-			pupils.Add(p5);
-			pupils.Add(p6);
-			pupils.Add(p7);
-
-			return pupils;
-		}
 
 		[Test]
 		public void DeleteAllLessonsForNextWeek()
@@ -178,7 +108,8 @@ namespace repetit
 				wait.Until(ExpectedConditions.StalenessOf(timetable));
 			}
 
-			Thread.Sleep(5000);
+			wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector("div.lesson")));
+			Thread.Sleep(500);
 			DeleteAllLessons();
 		}
 
